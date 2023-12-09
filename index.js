cff3dcf365299669c6fa330ae2c8da91e4cacb87
Guisposito <guicarlosjoinhas@gmail.com>
2023-12-09 12:39:34 -0300

    Iniciando o projeto

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+//Server config
 const express = require('express');
 const { resolve } = require('path');
-
 const app = express();
 const port = 3010;
+const http = require('http');
+const server = http.createServer(app);
 
+//Start socket
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//Setando pasta de arquivos estatics
 app.use(express.static('static'));
 
+const conn = io.of('/chat');
+
+conn.on('connection', (socket) => {
+  //recebendo e enviando a mensagem
+  socket.on('chatMessage', (data) => {
+    console.log('Recebi informações:', data);
+    socket.emit('retMsg', data);
+  })
+
+});
+
+
+//Apontando rotas
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+app.get('/chat', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/chatbox.html'));
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
